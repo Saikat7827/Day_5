8bcead8e9af48824010516af74e6cd5b9abccf8b
Saikat7827 <saikatchakraborty957.sc@gmail.com>
2022-09-07 22:49:12 +0530

    Swap Two Numbers

---
+++ Swap_Numbers.java
@@ -0,0 +1,26 @@
+package com.bl.Day5;
+
+import java.util.Scanner;
+
+public class Swap_Numbers {
+	public static void main(String[] args) {
+
+		int temp;
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter 1st number");
+		int num1 = sc.nextInt();
+
+		System.out.print("Enter 2nd number");
+		int num2 = sc.nextInt();
+
+		System.out.print("x=" + num1 + " and y=" + num2);
+
+		temp = num1;
+		num1 = num2;
+		num2 = temp;
+
+		System.out.println("After swapping: " + num1 + "   " + num2);
+		System.out.print("x=" + num1 + " and y=" + num2);
+		sc.close();
+	}
+}
