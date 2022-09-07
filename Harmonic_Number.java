679cec583be40976209c21097dc9a905c59c14fa
Saikat7827 <saikatchakraborty957.sc@gmail.com>
2022-09-07 22:32:48 +0530

    Harmonic Number

---
+++ Harmonic_Number.java
@@ -0,0 +1,22 @@
+package com.bl.Day5;
+
+import java.util.Scanner;
+
+public class Harmonic_Number {
+	public static void main(String[] args) {
+		double result = 0, i;
+
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter a number");
+		int num = sc.nextInt();
+
+		System.out.println("The Harmonic Value for the number " + num);
+		for (i = 1; i <= num; i++) {
+			result = (1 / i);
+			System.out.println(result);
+
+			sc.close();
+		}
+	}
+
+}
